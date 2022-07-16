44ea383f4de1238713fedcb6c15bc8aa0c14cc3a
ikyyybitzzxx <106673608+ikyyybitzzxx@users.noreply.github.com>
2022-07-17 02:08:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285822347348','6285822347348','6285822347348']
-global.premium = ['6285822347348']
+global.owner = ['6285922480867','6285922480867','6285922480867']
+global.premium = ['6285922480867']
 global.packname = 'Sticker by'
-global.author = 'ArullOfc'
+global.author = 'Kyy'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
